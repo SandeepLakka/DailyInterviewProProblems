7eb8181dcdf72a7935cd92487e8f86ff08f73c61
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2022-05-07 10:34:25 +0530

    Updated AnagramsFinder problem with more TCs

--- src/test/java/io/github/sandeeplakka/AnagramsFinderTest.java
+++ src/test/java/io/github/sandeeplakka/AnagramsFinderTest.java
@@ -15,7 +15,18 @@ class AnagramsFinderTest {
     }
 
     @Test
-    void find_anagrams() {
+    void find_anagrams_positive_cases() {
         assertArrayEquals(new int[]{3, 7}, anagramsFinder.find_anagrams("acdbacdacb", "abc"));
+        assertArrayEquals(new int[]{0, 4, 7}, anagramsFinder.find_anagrams("acdbacdacb", "ac"));
+        assertArrayEquals(new int[]{0, 6}, anagramsFinder.find_anagrams("cbaebabacd", "abc"));
+        assertArrayEquals(new int[]{0, 1, 2}, anagramsFinder.find_anagrams("abab", "ab"));
+    }
+
+    @Test
+    void find_anagrams_negative_cases() {
+        assertNull(anagramsFinder.find_anagrams(null, "a"));
+        assertNull(anagramsFinder.find_anagrams("abcdeabbcb", null));
+        assertNull(anagramsFinder.find_anagrams(null, null));
+        assertNull(anagramsFinder.find_anagrams("hello", "hello_there"));
     }
 }
