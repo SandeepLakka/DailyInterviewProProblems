f8be6de5efed921abbadda220a24ea1579121973
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2022-07-31 22:37:54 +0530

    Updated NumberPalindromeChecker with problem statement description

--- src/main/java/io/github/sandeeplakka/NumberPalindromeChecker.java
+++ src/main/java/io/github/sandeeplakka/NumberPalindromeChecker.java
@@ -1,5 +1,23 @@
 package io.github.sandeeplakka;
 
+/**
+ * This problem was recently asked by Twitter:
+ * <p>
+ * Given an integer, check if that integer is a palindrome.
+ * For this problem do not convert the integer to a string to check if it is a palindrome.
+ *
+ * <pre>
+ * import math
+ *
+ * def is_palindrome(n):
+ *   # Fill this in.
+ *
+ * print is_palindrome(1234321)
+ * # True
+ * print is_palindrome(1234322)
+ * # False
+ * </pre>
+ */
 public class NumberPalindromeChecker {
 
     public static void main(String[] args) {
