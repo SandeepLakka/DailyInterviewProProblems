7e3fb0aae04dd49b4a69d55021b8da310e92850d
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2022-08-06 22:38:50 +0530

    Added Base2Converter problem with impl and tests

---
+++ src/main/java/io/github/sandeeplakka/Base2Converter.java
@@ -0,0 +1,48 @@
+package io.github.sandeeplakka;
+
+/**
+ * This problem was recently asked by Apple:
+ * <p>
+ * Given a non-negative integer n, convert n to base 2 in string form.
+ * Do not use any built in base 2 conversion functions like bin.
+ * <p>
+ * Here's an example and some starter code:
+ * <pre>
+ * def base_2(n):
+ *   # Fill this in.
+ *
+ * print(base_2(123))
+ * # 1111011
+ *
+ *
+ * In the above example, 2^6 + 2^5 + 2^4 + 2^3 + 2^1 + 2^0 = 123.
+ * </pre>
+ */
+public class Base2Converter {
+    public static void main(String[] args) {
+        Base2Converter ut = new Base2Converter();
+
+        //1010
+        System.out.println(ut.convertToBase2(10));
+        //10
+        System.out.println(ut.convertToBase2(2));
+        //0
+        System.out.println(ut.convertToBase2(0));
+        //111
+        System.out.println(ut.convertToBase2(7));
+    }
+
+    public String convertToBase2(int number) {
+        /*
+
+         */
+        if (number == 0) return String.valueOf(0);
+        StringBuilder builder = new StringBuilder();
+        while (number > 0) {
+            builder.append(number % 2);
+            number /= 2;
+        }
+        return builder.reverse().toString();
+    }
+
+}
---
+++ src/test/java/io/github/sandeeplakka/Base2ConverterTest.java
@@ -0,0 +1,45 @@
+package io.github.sandeeplakka;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Base2ConverterTest {
+
+    private Base2Converter ut;
+    @BeforeEach
+    void setUp() {
+        ut = new Base2Converter();
+    }
+
+    @Test
+    void tests_convertToBase2_for_perfect_binary_numbers() {
+        assertEquals("0", ut.convertToBase2(0));
+        assertEquals("10", ut.convertToBase2(2));
+        assertEquals("100", ut.convertToBase2(4));
+        assertEquals("1000", ut.convertToBase2(8));
+        assertEquals("10000000", ut.convertToBase2(128));
+    }
+
+    @Test
+    void tests_convertToBase2_for_all_1s_numbers() {
+        assertEquals("1", ut.convertToBase2(1));
+        assertEquals("11", ut.convertToBase2(3));
+        assertEquals("111", ut.convertToBase2(7));
+        assertEquals("1111", ut.convertToBase2(15));
+        assertEquals("11111111", ut.convertToBase2(255));
+    }
+
+
+    @Test
+    void tests_convertToBase2_for_mix_of_1s_and_0s_numbers() {
+        assertEquals("1011", ut.convertToBase2(11));
+        assertEquals("101", ut.convertToBase2(5));
+        assertEquals("1100", ut.convertToBase2(12));
+        assertEquals("11110", ut.convertToBase2(30));
+        assertEquals("1101100110110", ut.convertToBase2(6966));
+    }
+
+
+}
