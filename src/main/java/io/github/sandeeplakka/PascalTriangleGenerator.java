7a7dd830f89472e8c6a3a6f16f16f28a14e0b58b
Sandeep Lakka <79764577+SandeepLakka@users.noreply.github.com>
2022-08-10 21:44:25 +0530

    Updated PascalTriangleGenerator to better time complexity instead of space.

--- src/main/java/io/github/sandeeplakka/PascalTriangleGenerator.java
+++ src/main/java/io/github/sandeeplakka/PascalTriangleGenerator.java
@@ -1,6 +1,8 @@
 package io.github.sandeeplakka;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.IntStream;
 
 /**
@@ -22,16 +24,24 @@ public class PascalTriangleGenerator {
     public static void main(String[] args) {
         PascalTriangleGenerator ut = new PascalTriangleGenerator();
         //[1, 5, 10, 10, 5, 1]
-        System.out.println(Arrays.toString(ut.getPascalTriangleRow(6)));
+        //System.out.println(Arrays.toString(ut.getPascalTriangleRow(6)));
     }
 
 
+    //Increased space to reduce time
     public int[] getPascalTriangleRow(int row) {
+        int[][] values = new int[row][row];
+        for (int[] arr : values) {
+            Arrays.fill(arr, -1);
+        }
         int n = row - 1;
-        return IntStream.rangeClosed(0, n).map(x -> getNcX(n, x)).toArray();
+        return IntStream.rangeClosed(0, n).map(x -> {
+            if (values[n][x] != -1) return values[n][x];
+            if (values[n][n - x] != -1) return values[n][n - x];
+            return values[n][x] = getNcX(n, x);
+        }).toArray();
     }
 
-    //Could be improved with cache
     private int getNcX(int n, int x) {
         int nominator = IntStream.rangeClosed(n - x + 1, n).reduce(1, (left, right) -> left * right);
         int denominator = IntStream.rangeClosed(1, x).reduce(1, (left, right) -> left * right);
